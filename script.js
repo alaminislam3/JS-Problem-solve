f9ae3cb99328c5900ce81a7fe6f61d932a79863d
Alamin Islam <alamin.ndc.17@gmail.com>
2025-08-18 14:36:48 +0600

    created a function which can reverse a string

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
-/* function reverseString (){
+function reverseString (str){
 
+  let arr = str.split("");
+  let reversedArr = arr.reverse()
+  let reverseStr = reversedArr.join("")
+  return reverseStr
+}
 
+const input = document.getElementById("inputString")
+const button = document.getElementById("reverseBtn")
+const output = document.getElementById("output")
 
-} */
+button.addEventListener("click", function(){
 
+  const userinput = input.value;
+  const reversed = reverseString(userinput)
+  output.textContent = reversed
+
+})
